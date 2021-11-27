6240df9bec68710fc9285963f53d7ee483b1c5cd
António Rocha <afgrocha98@gmail.com>
2021-11-27 00:03:41 +0000

    camera

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <LiveCamera/>
+</template>
+
+<script>
+import LiveCamera from './components/LiveCamera.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LiveCamera
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LiveCamera.vue
@@ -0,0 +1,214 @@
+<template>
+  <div id="app" class="web-camera-container">
+    <div class="camera-button">
+      <button
+        type="button"
+        class="button is-rounded"
+        :class="{ 'is-primary': !isCameraOpen, 'is-danger': isCameraOpen }"
+        @click="toggleCamera"
+      >
+        <span v-if="!isCameraOpen">Open Camera</span>
+        <span v-else>Close Camera</span>
+      </button>
+    </div>
+
+    <div v-show="isCameraOpen && isLoading" class="camera-loading">
+      <ul class="loader-circle">
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+
+    <div
+      v-if="isCameraOpen"
+      v-show="!isLoading"
+      class="camera-box"
+      :class="{ flash: isShotPhoto }"
+    >
+
+      <video ref="camera" :width="1080" :height="720" autoplay></video>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LiveCamera",
+  props: {
+    msg: String,
+  },
+  data: function () {
+    return {
+      isCameraOpen: false,
+      isPhotoTaken: false,
+      isShotPhoto: false,
+      isLoading: false,
+      link: "#",
+    };
+  },
+  methods: {
+    toggleCamera() {
+      if (this.isCameraOpen) {
+        this.isCameraOpen = false;
+        this.isPhotoTaken = false;
+        this.isShotPhoto = false;
+        this.stopCameraStream();
+      } else {
+        this.isCameraOpen = true;
+        this.createCameraElement();
+      }
+    },
+
+    createCameraElement() {
+      this.isLoading = true;
+
+      const constraints = (window.constraints = {
+        audio: false,
+        video: true,
+      });
+
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then((stream) => {
+          this.isLoading = false;
+          this.$refs.camera.srcObject = stream;
+        })
+        .catch(() => {
+          this.isLoading = false;
+          alert("May the browser didn't support or there is some errors.");
+        });
+    },
+
+    stopCameraStream() {
+      let tracks = this.$refs.camera.srcObject.getTracks();
+
+      tracks.forEach((track) => {
+        track.stop();
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body {
+  display: flex;
+  justify-content: center;
+}
+
+.web-camera-container {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 1080px;
+
+  .camera-button {
+    margin-bottom: 2rem;
+  }
+
+  .camera-box {
+    .camera-shutter {
+      opacity: 0;
+      width: 450px;
+      height: 337.5px;
+      background-color: #fff;
+      position: absolute;
+
+      &.flash {
+        opacity: 1;
+      }
+    }
+  }
+
+  .camera-shoot {
+    margin: 1rem 0;
+
+    button {
+      height: 60px;
+      width: 60px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 100%;
+
+      img {
+        height: 35px;
+        object-fit: cover;
+      }
+    }
+  }
+
+  .camera-loading {
+    overflow: hidden;
+    height: 100%;
+    position: absolute;
+    width: 100%;
+    min-height: 150px;
+    margin: 3rem 0 0 -1.2rem;
+
+    ul {
+      height: 100%;
+      position: absolute;
+      width: 100%;
+      z-index: 999999;
+      margin: 0;
+    }
+
+    .loader-circle {
+      display: block;
+      height: 14px;
+      margin: 0 auto;
+      top: 50%;
+      left: 100%;
+      transform: translateY(-50%);
+      transform: translateX(-50%);
+      position: absolute;
+      width: 100%;
+      padding: 0;
+
+      li {
+        display: block;
+        float: left;
+        width: 10px;
+        height: 10px;
+        line-height: 10px;
+        padding: 0;
+        position: relative;
+        margin: 0 0 0 4px;
+        background: #999;
+        animation: preload 1s infinite;
+        top: -50%;
+        border-radius: 100%;
+
+        &:nth-child(2) {
+          animation-delay: 0.2s;
+        }
+
+        &:nth-child(3) {
+          animation-delay: 0.4s;
+        }
+      }
+    }
+  }
+
+  @keyframes preload {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0.4;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+}
+</style>
