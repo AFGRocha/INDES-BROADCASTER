ce287d30c1cd7f8829345c8622ec45b307bb4d4b
arocha1 <arocha@infraspeak.com>
2021-12-17 19:16:27 +0000

    label fix

--- src/components/LocalVideoList.vue
+++ src/components/LocalVideoList.vue
@@ -23,7 +23,7 @@
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add Youtube Video</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Add Video</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
